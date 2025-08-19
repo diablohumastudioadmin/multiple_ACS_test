1b655694275ab192533645961dd40e1e40481c06
diablohumastudioadmin <admin@diablohumastudio.com>
2025-08-19 10:54:41 -0500

    unnecesary ref to acsunit deleted in Action. Only used in set_action

--- global_systems/action_condition_system/action.gd
+++ global_systems/action_condition_system/action.gd
@@ -5,6 +5,6 @@ enum TYPES {LVL_BUYED}
 var type: TYPES
 var payload: Payload
 
-func _init(_type: TYPES, _payload: Payload, acs_unit: ACS.ACSUnit = ACS.global_acs_unit) -> void:
+func _init(_type: TYPES, _payload: Payload) -> void:
 	type = _type
 	payload = _payload
